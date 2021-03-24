33de84e1c75096f050f46fa1fe0cbd6a04185da0
Liz <yehezkieldio35@gmail.com>
2021-03-24 14:30:05 +0800

    refactor: smooth scroll not working ._. :disappointed:

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,37 @@ document.querySelector(".toggle").addEventListener("click", function () {
 
     setTimeout(() => {
         this.classList.toggle("active");
-        document.documentElement.classList.toggle('theme-dark');
+        document.documentElement.classList.toggle("theme-dark");
     }, 150);
 
     setTimeout(() => this.classList.remove("animate"), 300);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-	const $navbarBurgers = Array.prototype.slice.call(
-		document.querySelectorAll(".navbar-burger"),
-		0
-	);
+    const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll(".navbar-burger"),
+        0
+    );
 
-	if ($navbarBurgers.length > 0) {
-		$navbarBurgers.forEach((el) => {
-			el.addEventListener("click", () => {
-				const target = el.dataset.target;
-				const $target = document.getElementById(target);
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach((el) => {
+            el.addEventListener("click", () => {
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
 
-				el.classList.toggle("is-active");
-				$target.classList.toggle("is-active");
-			});
-		});
-	}
+                el.classList.toggle("is-active");
+                $target.classList.toggle("is-active");
+            });
+        });
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
+    });
 });
